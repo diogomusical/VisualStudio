228bb2f0423619c40f3f6137d25f89470bb0808f
diogomusical <diogo.musical1@gmail.com>
2023-05-05 09:33:14 +0100

    criamos o projeto

---
+++ Contabancaria/ContaBancaria.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contabancaria
+{
+    internal class ContaBancaria
+    {
+        //Definição de propriedades
+        public int Numero { get; }
+        public string Titular { get; set; }
+
+        public double Saldo {  get; private set; }
+
+        //Construtores
+
+        //este construtor permite instanciar contas bancárias apenas com o nº de conta e o seu titular
+        public ContaBancaria(int numero, string titular) 
+        {
+            Numero = numero;
+            Titular = titular;
+        }
+
+        //Este construtor permite instanciar uma conta a partir do seu nº de conta
+        //titular e tb do seu saldo inicial)
+        public ContaBancaria(int numero, string titular, double saldo) : this(numero, titular)
+        {
+            Saldo = saldo;
+        } 
+
+        // Secção de Métodos
+
+        //Este é o método que permite depositar dinheiro na conta
+        public void Deposito(double quantia)
+        {
+            Saldo += quantia;
+        }
+
+        //Este é o método que permite fazer o levantamento de dinheiro
+        public void Levantamento (double quantia)
+        {
+            Saldo -= quantia + 5.00;
+        }
+
+        public override string ToString()
+        {
+            return "Conta Bancária número "
+                + Numero
+                + ", Titular da conta: "
+                + Titular
+                + ", Saldo "
+                + Saldo. ToString("F2")
+                + " euros.";
+        }
+    }
+}
---
+++ Contabancaria/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace Contabancaria
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaBancaria cb;
+
+            Console.WriteLine("Entre com o nº de conta: ");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine("Nome do titular da conta: ");
+            string titular = Console.ReadLine();
+            Console.WriteLine("Deseja fazer um depósito inicial?");
+            char resp = char.Parse(Console.ReadLine());
+            if (resp == 's' || resp == 'S')
+            {
+                Console.WriteLine("Qual é o valor de depósito inicial?");
+                double depositoInicial = double.Parse(Console.ReadLine());
+                cb = new ContaBancaria(numero, titular, depositoInicial);
+            }
+            else {  cb = new ContaBancaria(numero, titular); }
+
+            Console.WriteLine();
+            Console.WriteLine("Dados da Conta: ");
+            Console.WriteLine(cb);
+            Console.WriteLine();
+
+            Console.WriteLine("Valor a depositar: ");
+            double quantia = double.Parse(Console.ReadLine());
+            cb.Deposito(quantia);
+            Console.WriteLine("Dados atualizados da conta: ");
+            Console.WriteLine(cb);
+
+            Console.WriteLine();
+            Console.WriteLine("Que valor pretende levantar? ");
+            quantia = double.Parse(Console.ReadLine());
+            cb.Levantamento(quantia);
+            Console.WriteLine();
+            Console.WriteLine("Dados atualizados da conta: ");
+            Console.WriteLine(cb);
+
+            cb.Titular = "Pedro";
+           Console.WriteLine(cb);
+        }
+    }
+}
